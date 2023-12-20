b8059404751a9da3635445ed21a2b7ae441ed995
Maximiliano Gomez <alexander34223@gmail.com>
2023-12-20 18:04:25 -0300

    estructura del footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='bg-black text-center text-light py-4'>
+            <div>
+                <p>&copy;Todos Los derechos reservados</p>
+            </div>
+            
+        </footer>
+    );
+};
+
+export default Footer;
