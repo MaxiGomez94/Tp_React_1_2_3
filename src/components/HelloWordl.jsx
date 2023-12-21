fc996c74c8d0db2b8145ccfecf0fb2c3c802029e
Maximiliano Gomez <alexander34223@gmail.com>
2023-12-21 01:43:47 -0300

    agregue margen a los div

--- src/components/HelloWordl.jsx
+++ src/components/HelloWordl.jsx
@@ -1,27 +1,32 @@
-const HelloWordl = (props) => {
+
+
+const HelloWorld = props => {
+
+
   return (
     <section className="text-light fw-bold">
       <div className="mt-5">
-      <h3>Tp_1:</h3>
-      <p>
-        Crear una aplicación web con un componente que muestre en pantalla el
-        mensaje “Hello World”.
-      </p>
-      <ul>
-        <li>Hello World!!</li>
-      </ul>
+        <h3>Tp_1:</h3>
+        <p>
+          Crear una aplicación web con un componente que muestre en pantalla el
+          mensaje “Hello World”.
+        </p>
+        <ul>
+          <li>Hello World!!</li>
+        </ul>
       </div>
       <div className=" mt-5">
         <h3>Tp_2:</h3>
-        <p>Modificar el componente anterior y enviar mediante props el valor “My friend” de manera que el mensaje quede de la siguiente forma:</p>
+        <p>
+          Modificar el componente anterior y enviar mediante props el valor “My
+          friend” de manera que el mensaje quede de la siguiente forma:
+        </p>
         <ul>
-            <li>{props.myFriend}</li>
+          <li>{props.myFriend}</li>
         </ul>
       </div>
-     
-      
     </section>
   );
 };
 
-export default HelloWordl;
+export default HelloWorld;
